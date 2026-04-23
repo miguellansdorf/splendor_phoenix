09d9aacb2c2278f643d74ab7daf60a8554004db0
Miguel Lansdorf <miguellansdorf@gmail.com>
2026-04-23 12:17:52 +0800

    add dependencies for phoenix_vite

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule Splendor.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:igniter, "~> 0.6", only: [:dev, :test]},
       {:argon2_elixir, "~> 4.0"},
       {:phoenix, "~> 1.8.3"},
       {:phoenix_ecto, "~> 4.5"},
@@ -66,7 +67,8 @@ defmodule Splendor.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:phoenix_vite, "~> 0.4"}
     ]
   end
 
